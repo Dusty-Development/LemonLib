196f373592910c61fdfe00acbc07f6fcdb54b495
ConstantDust <Carteryackovich@gmail.com>
2025-02-15 10:16:35 -0500

    TryingToSetupMaven

--- src/main/java/net/dustley/lemon/modules/citrus_physics/component/collision/containers/ParticleColliderContainer.java
+++ src/main/java/net/dustley/lemon/modules/citrus_physics/component/collision/containers/ParticleColliderContainer.java
@@ -9,7 +9,7 @@ public class ParticleColliderContainer extends ColliderContainerComponent {
     public void solve(ActorComponent actor, double deltaTime) {
         // Use a simple n^2 algorithm for now... use a spatial grid later
         for (int i = 0; i < PhysicsWorld.CONSTRAINT_RESOLUTION; i++) {
-            ecsWorld.findEntitiesWith(ActorComponent.class, ConstraintComponent.class).stream().forEach(result -> result.comp2().solve(result.comp1(), deltaTime));
+//            ecsWorld.findEntitiesWith(ActorComponent.class, ConstraintComponent.class).stream().forEach(result -> result.comp2().solve(result.comp1(), deltaTime));
         }
     }
 
